393f21b8227e24b1446caa6e1673f746ce7d2a30
Dev_Avishka <dabdissanayake@gmail.com>
2023-08-18 07:42:39 +0530

    loll

--- src/Homepg.js
+++ src/Homepg.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './styles.css';
 import ImageWithText from './ImageWithText';
 import bm from './bm.png'
+import ftb from './ftb.png'
+import sr from './sr.png'
 function Homepg() {
   return (
     <div>
@@ -12,7 +14,9 @@ function Homepg() {
       <br></br>
       <br></br>
       <h3>
-        <a href='https://devavishka.itch.io/shattered-reflections'><img src={bm}></img></a>
+        <a href='https://devavishka.itch.io/blender-man'><img src={bm}></img></a>
+        <a href='https://devavishka.itch.io/find-the-bun'><img src={ftb}></img></a>
+        <a href='https://devavishka.itch.io/shattered-reflections'><img src={sr}></img></a>
       </h3>
     </div>
   );
